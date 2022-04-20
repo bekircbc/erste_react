29ff4c3f40fc1b720f1963d8805508e0d5537b10
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-20 09:46:05 +0200

    first site with react ceated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+import data from "./data/data.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App</h1>
+      <image src="../public/images/icon.png" alt="icon" />
+      <div>
+        First employee:{data.employees[0].firstName}{" "}
+        {data.employees[0].lastName}
+      </div>
+      <div>
+        Second employee:{data.employees[1].firstName}{" "}
+        {data.employees[1].lastName}
+      </div>
     </div>
   );
 }
